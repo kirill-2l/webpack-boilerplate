7f744f09ffc3a51f2def6d286ef79e877f1704f7
Петров Кирилл Александрович <petrovvkirill@gmail.com>
2021-04-28 18:32:04 +0300

    dev styles loaders

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,9 +3,17 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'eval-source-map',
   devServer: {
     contentBase: './dist',
     port: 9090,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
   },
 });
