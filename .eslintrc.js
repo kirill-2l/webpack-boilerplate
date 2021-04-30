f4d4824b6da37439a7c5de9b159fc181d2527ca3
Петров Кирилл Александрович <petrovvkirill@gmail.com>
2021-04-30 13:29:25 +0300

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
   rules: {
-    'import/no-extraneous-dependencies': [error, { devDependencies: true }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
   globals: {
     DEBUG: 'readonly',
