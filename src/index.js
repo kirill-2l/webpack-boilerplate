48ba4e86cdbe4962807d121c6d3499509ddabda0
Петров Кирилл Александрович <petrovvkirill@gmail.com>
2021-06-06 17:09:08 +0300

    hack for quick tailwind compilation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import logger from '@utils/logger';
 import './modules';
 import './components';
 
+/*
+ * hack for qucik tailwind compilation
+ * https://github.com/tailwindlabs/tailwindcss/issues/443#issuecomment-661559687
+ */
+
+import './styles/tailwind/before.css';
 import './styles/index.scss';
+import './styles/tailwind/after.css';
 
 logger.info('hello');
