1cecf92d024efde86bd86bba17d593131e5a46ef
kirill-2l <petrovvkirill@gmail.com>
2021-06-07 20:23:18 +0300

    fix tailwind build

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,14 @@ module.exports = {
     tailwindcss: {},
     'postcss-preset-env': {
       browsers: 'last 2 versions',
+
+      /*
+       * fix tailwind build error
+       * https://github.com/tailwindlabs/tailwindcss/issues/1190
+       */
+      features: {
+        'focus-within-pseudo-class': false,
+      },
     },
   },
 };
